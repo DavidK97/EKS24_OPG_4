45720813b8f9d9a82ee70b6dbe106e9e1179b7b3
David Kjærgaard <david.700@live.dk>
2024-12-27 13:47:06 +0100

    Add files via upload

---
+++ LaundryMachine.java
@@ -0,0 +1,11 @@
+public interface LaundryMachine {
+
+    void chooseProgram ();
+
+    void displayPriceAndDuration ();
+
+    void start ();
+
+    void addExtra ();
+}
+
---
+++ LaundryService.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+
+public class LaundryService {
+    private ArrayList<LaundryMachine> machines;
+
+    public LaundryService () {
+        this.machines = new ArrayList<>();
+    }
+
+
+    public void addMachine (LaundryMachine laundryMachine) {
+
+    }
+
+    public LaundryMachine chooseMachine () {
+
+        return null;
+    }
+
+}
